f8197e4d762d62247c0b31617625487883bf58de
Egor Z <egor@ezaderiy-mac.thumbtack.lo>
2015-05-15 15:04:34 +0600

    TextField and TextView

--- test-app/test-app/ViewController.swift
+++ test-app/test-app/ViewController.swift
@@ -10,14 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func buttonAction(sender: AnyObject) {
+        textView.text = textField.text
     }
 
 
